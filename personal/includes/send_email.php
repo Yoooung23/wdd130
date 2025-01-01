ae9706fe6d8184121041f8c428deeed2b3177230
Yoooung23 <logan7design@gmail.com>
2025-01-01 15:17:56 -0700

    meow

--- personal/includes/send_email.php
+++ personal/includes/send_email.php
@@ -4,15 +4,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $lastname = htmlspecialchars($_POST['lastname']);
     $phone = htmlspecialchars($_POST['phone']);
 
-    $to = "logan7design@gmail.com"; // Replace with your email address
-    $subject = "New Date Form Submission";
-    $message = "First Name: $firstname\nLast Name: $lastname\nPhone: $phone";
-    $headers = "From: DATEcage@gmail.com"; // Replace with a valid sender email
 
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Email sent successfully.";
-    } else {
-        echo "Email sending failed.";
-    }
+    echo "This is the data that the user submitted:";
+    echo "<br>First name: $firstname";
+    echo "<br>Last name: $lastname";
+    // $to = "logan7design@gmail.com"; // Replace with your email address
+    // $subject = "New Date Form Submission";
+    // $message = "First Name: $firstname\nLast Name: $lastname\nPhone: $phone";
+    // $headers = "From: DATEcage@gmail.com"; // Replace with a valid sender email
+
+    // if (mail($to, $subject, $message, $headers)) {
+    //     echo "Email sent successfully.";
+    // } else {
+    //     echo "Email sending failed.";
+    // }
 }
 ?>
